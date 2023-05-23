53d53662777e0b1c3b961204344a8c5bc808b099
ChristieLaf <133935258+ChristieLaf@users.noreply.github.com>
2023-05-23 15:50:37 -0400

    Major changes to JS file
    real-time weather can be seen

--- index.js/script.js
+++ index.js/script.js
@@ -17,7 +17,7 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
   let day = days[dayIndex];
 
@@ -26,16 +26,39 @@ function formatDate(date) {
 
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
 }
 
-function search(event) {
+function searchCity(city) {
+  let apiKey = "66d6ff1238907f6dafb724d6e10a9b10";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
-  let apiKey = "66d6ff1238907f6dafb724d6e10a9b10";
-  let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial';
-axios.get(apiUrl).then(displayWeatherCondition);
+  searchCity(city);
+}
 
+function searchLocation(position) {
+  let apiKey = "66d6ff1238907f6dafb724d6e10a9b10";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
 
 function convertToCelsius(event) {
   event.preventDefault();
@@ -56,7 +79,7 @@ dateElement.innerHTML = formatDate(currentTime);
 
 // Feature # 2-Week 4
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSubmit);
 
 // Bonus Feature-Week 4
 let celsiusLink = document.querySelector("#celsius-link");
@@ -64,3 +87,8 @@ celsiusLink.addEventListener("click", convertToCelsius);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
